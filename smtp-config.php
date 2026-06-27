fdcd1bff320d1a9f498fa6cac84d9b496ae9a90d
Azzbob <bonnetayoub@gmail.com>
2026-06-27 16:39:20 +0200

    Change SMTP password in smtp-config.php
    Updated SMTP password for application account.

--- smtp-config.php
+++ smtp-config.php
@@ -7,6 +7,6 @@
 define('SMTP_HOST',       'smtp.gmail.com');
 define('SMTP_PORT',       587);
 define('SMTP_USERNAME',   'azebob95@gmail.com');
-define('SMTP_PASSWORD',   'qhbegrpgelkpjymq'); // ⚠️ À METTRE À JOUR avec le mot de passe d'application du compte azebob95
+define('SMTP_PASSWORD',   'oycq hrsp fqdj qjcu'); // ⚠️ À METTRE À JOUR avec le mot de passe d'application du compte azebob95
 define('SMTP_FROM_EMAIL', 'azebob95@gmail.com');
 define('SMTP_FROM_NAME',  'Liens Démarches');
